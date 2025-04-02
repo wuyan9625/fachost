15ee89f14928574610ee742ebb86c9aa973bbcff
wuyan9625 <wuyan9625@gmail.com>
2025-04-02 16:30:27 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,11 +14,11 @@ function initNavBar() {
   if (role === 'admin') {
     // 如果是管理員，導航到管理員控制台頁面
     controlPanelLink.href = "admin-dashboard.html";
-    controlPanelLink.textContent = "admin控制台";
+    controlPanelLink.textContent = "admin_console";
   } else {
     // 如果是普通用戶，導航到普通用戶控制台頁面
     controlPanelLink.href = "dashboard.html";
-    controlPanelLink.textContent = "uesr控制台";
+    controlPanelLink.textContent = "console";
   }
 
   navLinks.forEach(link => {
