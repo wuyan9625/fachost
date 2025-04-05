7d2fe28e1dde2a5b88320c4efb022b0a89a59734
wuyan9625 <wuyan9625@gmail.com>
2025-04-05 23:43:21 +0800

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -75,7 +75,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (isLogin) {
       // 登入
       try {
-        const res = await fetch("/api/login", {
+        const res = await fetch('https://fachost.cloud/api/login', {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ email, password }),
